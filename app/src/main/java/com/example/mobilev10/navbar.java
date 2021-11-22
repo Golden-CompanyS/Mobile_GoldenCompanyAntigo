e923163971a36b7f5957fa3881d16d52394d4e43
LarissaSonoda <larissa.sonoda@etec.sp.gov.br>
2021-11-22 07:06:17 -0300

    navbar e tela do usuário - front

---
+++ app/src/main/java/com/example/mobilev10/navbar.java
@@ -0,0 +1,14 @@
+package com.example.mobilev10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class navbar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navbar);
+    }
+}
