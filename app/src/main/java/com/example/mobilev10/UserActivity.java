4a182262145527c2460c92c7b4cc921aae9e8720
LarissaSonoda <larissa.sonoda@etec.sp.gov.br>
2021-11-28 20:38:13 -0300

    geolocalização

--- app/src/main/java/com/example/mobilev10/UserActivity.java
+++ app/src/main/java/com/example/mobilev10/UserActivity.java
@@ -1,12 +1,20 @@
 package com.example.mobilev10;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.SearchManager;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -14,20 +22,53 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
-public class UserActivity extends AppCompatActivity {
+public class UserActivity extends AppCompatActivity implements FetchAddressTask.OnTaskCompleted {
 
     EditText edtNotes;
     Button btnSave;
     ImageView imgUser;
     Button btnAltPerfil;
     ImageView imgPerfil;
+
+    public static final String PREFERENCIAS_NAME = "com.example.android.localizacao";
+    private static final String TRACKING_LOCATION_KEY = "tracking_location";
+    // Constantes
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+    private static final String LASTADRESS_KEY = "adress";
+    private static final String LATITUDE_KEY = "latitude";
+    private static final String LONGITUDE_KEY = "longitude";
+
+    // Shared preferences
+    private SharedPreferences mPreferences;
+    private String lastLatitude = "";
+    private String lastLongitude = "";
+    private String lastAdress = "";
+
+
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LocationCallback mLocationCallback;
+    private TextView mLocationTextView;
+
+    private boolean mTrackingLocation;
+
     private int EXTERNAL_STORAGE_PERMISSION_CODE = 23;
     private static final int RESULT_SELECT_IMAGE = 1;
     @Override
@@ -39,8 +80,38 @@ public class UserActivity extends AppCompatActivity {
         btnSave= (Button) findViewById(R.id.btnSaveNotes);
         imgUser = (ImageView) findViewById(R.id.imgUser);
         btnAltPerfil = (Button) findViewById(R.id.btnAltFoto);
+        mLocationTextView = (TextView) findViewById(R.id.txtEndereco);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(
+                this);
+
+        // Recupera o estado da aplicação quando é recriado
+        if (savedInstanceState != null) {
+            mTrackingLocation = savedInstanceState.getBoolean(
+                    TRACKING_LOCATION_KEY);
+        }
+        mLocationCallback = new LocationCallback() {
+            /**
+             * This is the callback that is triggered when the
+             * FusedLocationClient atualiza a localização.
+             * @param locationResult The result containing the device location.
+             */
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                // If tracking is turned on, reverse geocode into an address
+                if (mTrackingLocation) {
+                    new FetchAddressTask(UserActivity.this, UserActivity.this)
+                            .execute(locationResult.getLastLocation());
+                }
+            }
+        };
 
 
+
+        //inicializa as preferências do usuário
+        mPreferences = getSharedPreferences(PREFERENCIAS_NAME, MODE_PRIVATE);
+        //recupera as preferencias
+        //recuperar();
     }
 //SALVAR AS ANOTAÇÕES - ARMAZENAMENTO EXTERNO
     public void savePublicly(View view){
@@ -145,5 +216,112 @@ public class UserActivity extends AppCompatActivity {
         this.overridePendingTransition(0, 0);
         finish();
     }
+
+    //LOCALIZAÇÃO
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_LOCATION_PERMISSION:
+                // Permissão garantida
+                if (grantResults.length > 0
+                        && grantResults[0]
+                        == PackageManager.PERMISSION_GRANTED) {
+                    iniciarLocal();
+                } else {
+                    Toast.makeText(this,
+                            R.string.permissao_negada,
+                            Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    private LocationRequest getLocationRequest() {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_LOW_POWER);
+        return locationRequest;
+    }
+    @SuppressLint("StringFormatMatches")
+    private void iniciarLocal() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION_PERMISSION);
+        } else {
+            mTrackingLocation = true;
+            mFusedLocationClient.requestLocationUpdates
+                    (getLocationRequest(),
+                            mLocationCallback,
+                            null /* Looper */);
+            mFusedLocationClient.getLastLocation().addOnCompleteListener(
+                    task -> {
+                        Location location = task.getResult();
+                        if (location != null) {
+                            Geocoder geocoder = new Geocoder(UserActivity.this,
+                                    Locale.getDefault());
+                            try {
+                                List<Address> adresses = geocoder.getFromLocation(
+                                        location.getLatitude(), location.getLongitude(), 1);
+                                adresses.get(0).getCountryName();
+                                SharedPreferences preferences = getSharedPreferences(PREFERENCIAS_NAME, 0);
+
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                    });
+            mLocationTextView.setText(getString(R.string.endereco_text,
+                    getString(R.string.loading), null, null));
+        }
+    }
+    @SuppressLint("StringFormatMatches")
+    @Override
+    public void onTaskCompleted(String[] result) {
+        if (mTrackingLocation) {
+            // Update the UI
+            lastAdress = result[0];
+            lastLatitude = result[1];
+            lastLongitude = result[2];
+            mLocationTextView.setText(getString(R.string.endereco_text));
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean(TRACKING_LOCATION_KEY, mTrackingLocation);
+        super.onSaveInstanceState(outState);
+    }
+
+    //Armazena as preferencias do usuário
+    //na aplicação será armazenada a última localização
+
+    private void armazenar(String latitude, String longitude, String lastAdress) {
+        SharedPreferences.Editor preferencesEditor = mPreferences.edit();
+        preferencesEditor.putString(LASTADRESS_KEY, lastAdress);
+        preferencesEditor.apply();
+    }
+
+
+    @Override
+    protected void onResume() {
+        if (mTrackingLocation) {
+            iniciarLocal();
+        }
+        recuperar();
+        super.onResume();
+    }
+
+    @SuppressLint("StringFormatMatches")
+    private void recuperar() {
+        SharedPreferences mPreferences = getSharedPreferences(PREFERENCIAS_NAME, 0);
+        lastAdress = mPreferences.getString(LASTADRESS_KEY, "");
+        Toast.makeText(this, getString(R.string.endereco_text), Toast.LENGTH_SHORT).show();
+    }
+
+
 
 }
---
+++ app/src/main/java/com/example/mobilev10/localizacao.java
@@ -0,0 +1,33 @@
+package com.example.mobilev10;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+
+public class localizacao implements LocationListener {
+    public static double latitude;
+    public static double longitude;
+
+    @Override
+    public void onLocationChanged(Location location){
+        this.latitude = location.getLatitude();
+        this.longitude = location.getLongitude();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider){
+
+    }
+}
